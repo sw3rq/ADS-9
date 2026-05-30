6330712e7597c6b24b16591b7fc61a61ad9c4441
sw3rq <layza2k13@gmail.com>
2026-05-31 01:58:13 +0300

    Update tree.h

--- include/tree.h
+++ include/tree.h
@@ -1,31 +1,29 @@
-// Copyright 2022 NNTU-CS
+// Copyright 2022 NNTR-CS
 #ifndef INCLUDE_TREE_H_
 #define INCLUDE_TREE_H_
-
 #include <vector>
 
 class PMTree {
  public:
-    struct Node {
-        char sym;
-        std::vector<Node*> links;
-        explicit Node(char s) : sym(s) {}
-    };
+  struct Node {
+    char sym;
+    std::vector<Node*> links;
+    explicit Node(char s) : sym(s) {}
+  };
+  Node* top;
+  std::vector<char> base;
 
-    Node* top;
-    std::vector<char> base;
-
-    explicit PMTree(const std::vector<char>& src);
-    ~PMTree();
+  explicit PMTree(const std::vector<char>& src);
+  ~PMTree();
 
  private:
-    Node* generate(const std::vector<char>& rest);
-    void destroy(Node* ptr);
+  Node* generate(const std::vector<char>& rest);
+  void destroy(Node* ptr);
 };
 
 std::vector<std::vector<char>> getAllPerms(PMTree& obj);
 std::vector<char> getPerm1(PMTree& obj, int pos);
 std::vector<char> getPerm2(PMTree& obj, int pos);
-
 size_t fact(int n);
+
 #endif  // INCLUDE_TREE_H_
